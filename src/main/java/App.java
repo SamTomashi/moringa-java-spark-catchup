1295f3db5810991dd5e9b11b5d735f24f06a1239
Sam Tomashi <samtomashikayembi@yahoo.fr>
2020-11-30 12:27:50 +0300

    Implement routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        get("/hello", (request, response) ->
+
+                "<p><a href='/favorite_photos' >P.S. Check out my favorite travel photos here.</a></p>"
+
+        );
+
+
+        get("/favorite_photos", (request, response) ->
+                "Here are my photos"
+        );
+    }
 }
